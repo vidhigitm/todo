7197093c09af5f5eb4e8118dfe2dcf5d09f22742
Vidhi Makanwar <vidhimakanwar18@gmail.com>
2025-07-08 11:47:33 +0530

    Initial commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const tdBlue = Color(0xFF5F52EE);
+
+const tdBlack = Color(0xFF3A3A3A);
+const tdGrey = Color(0xFF717171);
+
+const tdBGColor = Color(0xFFEEEFF5);
